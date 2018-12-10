cb74e627add8c3d2f3e9a3b9b7b0dcbc238aab1d
David Richardson <david@tankski.co.uk>
2018-12-10 15:40:59 +0000

    Create a simulation script javascript file

---
+++ tpwsn-trickle/sim-script.js
@@ -0,0 +1,10 @@
+/**
+ * TPWSN Trickle Simulation Script
+ * 
+ * Created by David Richardson, University of Warwick
+ */
+
+ // TODO: Get the list of motes, set one to be a source and another a sink
+ // TODO: Get the set of motes that can fail and define a maximal failure count at any given time
+ // TODO: Figure out a way to write the output to a singular file - the output could be multiple
+ // but that wouldn't be a very nice way to do things.
