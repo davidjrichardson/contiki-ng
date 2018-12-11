2e05118ec1a8e3eb86b9b4e9068bd52bb33f5cb7
David Richardson <david@tankski.co.uk>
2018-12-11 14:03:15 +0000

    Start work on the simulation script

--- tpwsn-trickle/sim-script.js
+++ tpwsn-trickle/sim-script.js
@@ -4,7 +4,46 @@
  * Created by David Richardson, University of Warwick
  */
 
- // TODO: Get the list of motes, set one to be a source and another a sink
- // TODO: Get the set of motes that can fail and define a maximal failure count at any given time
- // TODO: Figure out a way to write the output to a singular file - the output could be multiple
- // but that wouldn't be a very nice way to do things.
+// TODO: Figure out a way to write the output to a singular file - the output could be multiple
+// but that wouldn't be a very nice way to do things.
+
+// TODO: Store the failed motes when they fail and remove them when they come back up
+// TODO: Specify the timeout and recovery delay in the script
+
+// The maximum number of nodes that can fail at once
+max_failure_count = 1
+// The recovery delay in clock ticks
+mote_recovery_delay = 500
+// The total simulation timeout
+
+all_motes = sim.getMotes();
+failed_motes = new Array(max_failure_count);
+
+// The source and sink node IDs - they cannot be the same.
+source_id = Math.floor(Math.random() * all_motes.length);
+sink_id = Math.floor(Math.random() * all_motes.length);
+
+// Make sure that the source and sink aren't the same
+while (source_id == sink_id) {
+   sink_id = Math.floor(Math.random() * all_motes.length);
+}
+
+// Actually get the mote(s) and tell them that they're these nodes
+source_node = all_motes[source_id];
+sink_node = all_motes[sink_id];
+write(source_node, "set source");
+write(sink_node, "set sink");
+
+// Create a failable motes array
+failable_motes = new Array(all_motes.length - 2);
+for (var i = 0; i < all_motes.length; i++) {
+    if (all_motes[i] == source_node | all_motes[i] == sink_node) {
+        failable_motes.push(all_motes[i]);
+    }
+}
+
+while (true) {
+    // TODO: This is where the main sim loop sits
+    // Need to update and track the failed mote(s), as well as write the mote output
+    // to file(s)
+}
