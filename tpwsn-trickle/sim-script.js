3c88034319fb529347950677aa7455d7fe6ec484
David Richardson <david@tankski.co.uk>
2018-12-12 14:50:44 +0000

    Fix an issue with simulation script
    The TIMEOUT function requires a literal timeout - it will not accept a variable. :(

--- tpwsn-trickle/sim-script.js
+++ tpwsn-trickle/sim-script.js
@@ -6,15 +6,19 @@
 
 // TODO: Figure out a way to write the output to a singular file - the output could be multiple
 // but that wouldn't be a very nice way to do things.
-
 // TODO: Store the failed motes when they fail and remove them when they come back up
+
+// importPackage(java.io);
 
 // The maximum number of nodes that can fail at once
 max_failure_count = 1
 // The recovery delay in clock ticks
 mote_recovery_delay = 500
-// The total simulation timeout
-simulation_timeout = 10000
+
+// Simulation file output object
+outputs = new Object();
+// Simulation log file prefix
+file_prefix = "log_"
 
 all_motes = sim.getMotes();
 failed_motes = new Array(max_failure_count);
@@ -31,26 +35,46 @@ while (source_id == sink_id) {
 // Actually get the mote(s) and tell them that they're these nodes
 source_node = all_motes[source_id];
 sink_node = all_motes[sink_id];
-write(source_node, "set source");
-write(sink_node, "set sink");
+
+log.log("src: " + source_node + "\n");
+log.log("snk: " + sink_node + "\n");
 
 // Create a failable motes array
 failable_motes = new Array(all_motes.length - 2);
 for (var i = 0; i < all_motes.length; i++) {
-    if (all_motes[i] == source_node | all_motes[i] == sink_node) {
+    if (i != source_id | i != sink_id) {
         failable_motes.push(all_motes[i]);
     }
 }
 
-TIMEOUT(simulation_timeout, log.log("Simulation terminated"));
+log.log(failable_motes);
+
+GENERATE_MSG(2000, "start-sim");
+YIELD_THEN_WAIT_UNTIL(msg.equals("start-sim"));
+
+write(source_node, "set source");
+write(sink_node, "set sink");
+
+TIMEOUT(10000, log.log("\n\nfoo\n"));
+
 while (true) {
     // TODO: This is where the main sim loop sits
     // Need to update and track the failed mote(s), as well as write the mote output
     // to file(s)
 
+    // Write the output for each mote to a file
+    // if (!outputs[id.toString()]) {
+    //     outputs[id.toString()] = new FileWriter(file_prefix + id + ".txt");
+    // }
+    // outputs[id.toString()].write(time + ";" + msg + "\n");
+
     try {
         YIELD();
     } catch (e) {
+        // for (var ids in outputs) {
+        //     outputs[ids].close();
+        // }
+
         throw("Simulation script killed")
     }
 }
