138253768b3e5157daf44a92ea30aa4fdd561684
David Richardson <david@tankski.co.uk>
2019-02-05 13:45:22 +0000

    Update parts of the script with Cooja modifications

--- tpwsn-trickle/sim-script.js
+++ tpwsn-trickle/sim-script.js
@@ -3,13 +3,10 @@
  * 
  * Created by David Richardson, University of Warwick
  */
-
-// TODO: Figure out a way to write the output to a singular file - the output could be multiple
-// but that wouldn't be a very nice way to do things.
-// TODO: Store the failed motes when they fail and remove them when they come back up
 load("nashorn:mozilla_compat.js");
 importPackage(java.io);
 importPackage(java.util);
+importPackage(org.contikios.cooja.util);
 
 // Java types
 var ArrayList = Java.type("java.util.ArrayList");
@@ -18,6 +15,13 @@ var ArrayList = Java.type("java.util.ArrayList");
 var max_failure_count = 1;
 // The recovery delay in clock ticks
 var mote_recovery_delay = 500;
+// The failure probability for a single node (1/this value)
+var mote_failure_probability = 100;
+
+// The random generator for failing motes (tied to the sim seed)
+var failure_rand = new Random(sim.getRandomSeed());
+// The failure mode for this simulation
+var failure_mode = "location";
 
 // Simulation file output object
 var outputs = new Object();
@@ -25,7 +29,7 @@ var outputs = new Object();
 var file_prefix = "log_";
 
 var all_motes = sim.getMotes();
-var failed_motes = new Array(max_failure_count);
+var failed_motes = new ArrayList(max_failure_count);
 
 // The source and sink node IDs - they cannot be the same.
 var source_id = Math.floor(Math.random() * all_motes.length);
@@ -44,12 +48,15 @@ log.log("src: " + source_node + "\n");
 log.log("snk: " + sink_node + "\n");
 
 // Create a failable motes array
-var failable_motes = new Array(all_motes.length - 2);
+var failable_motes = new ArrayList(all_motes.length - 2);
 for (var i = 0; i < all_motes.length; i++) {
     if (i !== source_id && i !== sink_id) {
         failable_motes.push(all_motes[i]);
     }
 }
+
+// Create the NodeGraph instance
+var nodeGraph = new NodeGraph(sim);
 
 // TODO: When a failure occurs:
 // * Choose a set of nodes that are relevant to the failure mode
@@ -71,43 +78,23 @@ write(sink_node, "set sink");
 TIMEOUT(10000, log.log("\n\nfoo\n"));
 
 /**
- * Function to get the 1-hop neighbourhood of a given mote in the simulation
- * @param mote - The mote that the neighbourhood is of
- * @returns An ArrayList of motes that are in the 1-hop neighbourhood of mote
- */
-function getNeighbourhood(mote) {
-    var position = mote.getInterfaces().getPosition();
-    var motes = sim.getMotes();
-
-    // Sort the motes based on distance to the current mote
-    Arrays.sort(motes, function (a, b) {
-        var keyA = position.getDistanceTo(a),
-            keyB = position.getDistanceTo(b);
-
-        return keyA - keyB;
-    });
-
-    // Filter the motes by transmission distance (50m) and make sure it's not the
-    // mote being called upon
-    var filteredMotes = new ArrayList();
-    for each (var m in motes) {
-        if (position.getDistanceTo(m) <= 50 && mote !== m) {
-            filteredMotes.add(m);
-        }
-    }
-
-    return filteredMotes;
-}
-
-/**
  * A high-level function to fail node(s) in the simulation based on a specified failure mode
  * @param failureMode - The failure mode (String) to determine how to fail node(s).
  */
-function failNodes(failureMode) {
+function failNode(failureMode) {
+    // Check if we can fail any more nodes and return early if not
+    if ((max_failure_count - failed_motes.length) > 0) {
+        return;
+    }
+
+    // TODO: Log which mote(s) are being failed
+
     if (failureMode === "location") {
         // TODO: Pick a node to fail -- either 1 hop neighbour of current or a random node if not
     } else if (failureMode === "temporal") {
         // TODO: Pick a note at random and schedule another failure (with increased likelihood)
+    } else if (failureMode === "random") {
+        // TODO: Just randomly pick a mote to crash
     }
 }
 
@@ -126,6 +113,10 @@ while (true) {
 
     try {
         YIELD();
+
+        if (failure_rand.nextInt(mote_failure_probability) === 0) {
+            failNode(failure_mode);
+        }
     } catch (e) {
         for (var ids in outputs) {
             log.log("Closing filewriter for id " + ids + "\n");
